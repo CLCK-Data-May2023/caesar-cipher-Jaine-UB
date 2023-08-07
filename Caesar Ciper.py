a4a6a47393b91868ffedbae72f137b08edcfa447
Unurjargal Boldbaatar <aja20.aja10success@gmail.com>
2023-08-06 22:18:58 -0400

    Adding caesar ciper code

---
+++ Caesar Ciper.py
@@ -0,0 +1,45 @@
+encryption = {
+    "a" : "f",
+    "b" : "g",
+    "c" : "h",
+    "d" : "i",
+    "e" : "j",
+    "f" : "k",
+    "g" : "l",
+    "h" : "m",
+    "i" : "n",
+    "j" : "o",
+    "k" : "p",
+    "l" : "q",
+    "m" : "r",
+    "n" : "s",
+    "o" : "t",
+    "p" : "u",
+    "q" : "v",
+    "r" : "w",
+    "s" : "x",
+    "t" : "y",
+    "u" : "z",
+    "v" : "a",
+    "w" : "b",
+    "x" : "c",
+    "y" : "d",
+    "z" : "e"
+    }
+
+
+
+normal_sentence = input("Please enter a sentence: ").lower()
+print(normal_sentence)
+
+encrypted_sentence = ''
+
+for letter in normal_sentence:
+    if letter in encryption:
+        encrypted_sentence += encryption[letter]
+    else:
+        encrypted_sentence += letter
+
+print(encrypted_sentence)
+        
+        
